bc9f40a94feb83bea900d40c1ef77975d47e86cf
ahin mukherjee <mukherjeeahin47@gmail.com>
2025-04-23 22:07:40 +0530

    modified

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,58 +1,97 @@
 import React from 'react'
 import "../Styles/home.css";
+import blackcar from "../images/blackcar.png";
+import boat from "../images/boat.png"
+import butterfly from "../iamges/butterfly.png";
+import car from "../images/car.png";
+import cars from "../images/cars.png";
 
 const home = () => {
   return (
-    <>
-      <div cclassName="box-body">
-         <div className="box">
-            <div className="content">
-                <h2>Watch</h2>
-                <div class="box-image" style="background-image: url('https://hodinkee.imgix.net/uploads/images/28f4bb3a-c1dc-4ded-b06a-3d7230f637b9/20221205_AppleWatchUltra_190.jpg?ixlib=rails-1.1.0&fm=jpg&q=55&auto=format&usm=12&fit=crop&ch=Width%2CDPR%2CSave-Data&alt=&ar=16%3A9&w=1440');">
-                </div>
+    <div className="body">
+        <div className="slider-container">
+          <div className="slider">
+            <div class="slide">
+              <iamge src={blackcar}/>
+            </div>
+            <div className="slide">
+              <iamge src={boat}/>
+            </div>
+            <div className="slide">
+              <iamge src={butterfly}/>
+            </div>
+            <div className="slide">
+              <iamge src={cars}/> 
             </div>
          </div>
-         <div className="box">
-            <div className="content">
-                <h2>Shoses</h2>
-                <div className="box-image" style="background-image: url('https://hips.hearstapps.com/hmg-prod/images/hoka-zinal-13085-1643565794.jpg?crop=1.00xw:0.849xh;0,0.151xh&resize=2048:*');">
-                </div>
-            </div>
-         </div> 
+        </div>
 
-         <div className="box">
-            <div className="content">
-            <h2>Headphone</h2>
-            <div className="box-image" style="background-image: url('https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Headphones-Lead-Image.png?resize=1080,720');">
-            </div>
-          </div>
-          </div>
+        <div class="box-body">
 
-          <div className="box">
-            <div className="content">
-            <h2>Headphone</h2>
-            <div className="box-image" style="background-image: url('https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Headphones-Lead-Image.png?resize=1080,720');">
-            </div>
+        <div class="box">
+          <div class="box-image" style="background-image: url('https://via.placeholder.com/300x200?text=Watch');"></div>
+          <div class="content">
+            <h2>Smart Watch</h2>
+            <p class="details">Waterproof, GPS, 48-hour battery life.</p>
+            <div class="rating">¡ï¡ï¡ï¡ï¡î</div>
+            <button class="btn-cart">Add to Cart</button>
           </div>
-          </div>
+        </div>
 
-         <div className="box">
-            <div className="content">
-            <h2>Headphone</h2>
-            <div className="box-image" style="background-image: url('https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Headphones-Lead-Image.png?resize=1080,720');">
-            </div>
+        <div class="box">
+          <div class="box-image" style="background-image: url('https://via.placeholder.com/300x200?text=Shoes');"></div>
+          <div class="content">
+            <h2>Running Shoes</h2>
+            <p class="details">Breathable fabric, shock absorption.</p>
+            <div class="rating">¡ï¡ï¡ï¡ï¡ï</div>
+            <button class="btn-cart">Add to Cart</button>
           </div>
-          </div>
+        </div>
 
-         <div className="box">
-            <div className="content">
-            <h2>Headphone</h2>
-            <div className="box-image" style="background-image: url('https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Headphones-Lead-Image.png?resize=1080,720');">
-            </div>
+        <div class="box">
+          <div class="box-image" style="background-image: url('https://via.placeholder.com/300x200?text=Headphone');"></div>
+          <div class="content">
+            <h2>Wireless Headphones</h2>
+            <p class="details">Noise cancelling, 20hr battery, Bluetooth 5.0</p>
+            <div class="rating">¡ï¡ï¡ï¡ï¡î</div>
+            <button class="btn-cart">Add to Cart</button>
           </div>
+        </div>
+
+        <div class="box">
+          <div class="box-image" style="background-image: url('https://via.placeholder.com/300x200?text=Headphone');"></div>
+          <div class="content">
+            <h2>Gaming Headset</h2>
+            <p class="details">360¡ã sound, mic, RGB lighting.</p>
+            <div class="rating">¡ï¡ï¡ï¡î¡î</div>
+            <button class="btn-cart">Add to Cart</button>
           </div>
-     </div>
-    </>
+        </div>
+
+        <div class="box">
+          <div class="box-image" style="background-image: url('https://via.placeholder.com/300x200?text=Headphone');"></div>
+          <div class="content">
+            <h2>Studio Headphones</h2>
+            <p class="details">High-fidelity audio, over-ear.</p>
+            <div class="rating">¡ï¡ï¡ï¡ï¡ï</div>
+            <button class="btn-cart">Add to Cart</button>
+          </div>
+        </div>
+
+        <div class="box">
+          <div class="box-image" style="background-image: url('https://via.placeholder.com/300x200?text=Headphone');"></div>
+          <div class="content">
+            <h2>Foldable Headphones</h2>
+            <p class="details">Portable, wireless, deep bass.</p>
+            <div class="rating">¡ï¡ï¡ï¡ï¡î</div>
+            <button class="btn-cart">Add to Cart</button>
+          </div>
+        </div>
+
+        </div>
+
+    </div>
+  
   )
 }
 
